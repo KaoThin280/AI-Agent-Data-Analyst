bf1459b8e467200c249c3541048861f147763237
DragonFly7 <157336798+KaoThin280@users.noreply.github.com>
2026-05-18 13:36:56 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import MainLayout from './components/Layout/MainLayout';
+import MainLayout from "./components/layout/MainLayout";
 export default function App() {
   return <MainLayout />;
 }
